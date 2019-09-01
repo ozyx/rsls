77d12a965bd5b7cb011e08b68c6ee41df82563be
Jesse Mazzella <jessemazzella@gmail.com>
2019-09-01 12:17:40 -0700

    basic functionality

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 extern crate clap;
 use clap::{App, Arg};
+use std::path::Path;
+use std::fs::{self};
+use std::io;
 
 fn main() {
     let matches = App::new("rsls")
@@ -15,4 +18,15 @@ fn main() {
                     .get_matches();
     
     let path = matches.value_of("path").unwrap_or(".");
+    list(Path::new(path));
+}
+
+fn list(path: &Path) -> io::Result<()> {
+    if path.is_dir() {
+        for entry in fs::read_dir(path)? {
+            let entry = entry?;
+            println!("{}", entry.file_name().to_str().unwrap());
+        }
+    }
+    Ok(())
 }
