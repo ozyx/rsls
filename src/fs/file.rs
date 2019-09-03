eb7655a41a8a31fc99f0504d2b1a43d01e6f8a80
Jesse Mazzella <jessemazzella@gmail.com>
2019-09-03 13:39:09 -0700

    create File

---
+++ src/fs/file.rs
@@ -0,0 +1,25 @@
+use std::fs::{self};
+use std::path::PathBuf;
+
+pub struct File {
+    /// The filename
+    pub name: String,
+    /// The file extension
+    pub ext: Option<String>,
+    /// The file path
+    pub path: PathBuf,
+    /// The file's metadata
+    pub metadata: fs::Metadata,
+}
+
+impl File {
+    /// Whether this file is a directory on the filesystem.
+    pub fn is_directory(&self) -> bool {
+        self.metadata.is_dir()
+    }
+
+    /// Whether this file is a regular file on the filesystem
+    pub fn is_file(&self) -> bool {
+        self.metadata.is_file()
+    }
+}
